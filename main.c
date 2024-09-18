e972e604f034b547748620f30b45f7ea3e18eb07
Lucas de Oliveira <167724471+lucasdeoliveiralima@users.noreply.github.com>
2024-09-18 12:32:34 -0300

    Definição da struct

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct processador{
+    char modelo[25];
+    int numero_cores;
+    int numero_threads;
+    int litografia_nm;
+    float clock_basico_ghz;
+    float clock_maximo_ghz;
+    int tdp_watts;
+    char video_integrado;
+}Processador;    
+
+int main() {
+        
+    return 0;
+}
